478751a3e12a6508869ff5c8dc4e8447885d44d8
Monke <gerard.busca@enti.cat>
2024-05-05 19:48:57 +0200

    Now it reads from config
    Config doesn't work properly and the oppenig of zones don't work at all

--- PokeEnti/Source.cpp
+++ PokeEnti/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <conio.h>
 #include <ctime>
+#include <string>
+#include <fstream>
 #include "HeaderPokeenti.h"
 #define MAPSIZE 20
 #define MAXPOKEMON 2
@@ -9,21 +11,71 @@
 #define KEY_RIGHT 77
 #define KEY_LEFT 75
 #define KEY_SPACE 32
-
+using namespace std;
 
 void main()
 {
 	srand(time(NULL));
 	char mapa[MAPSIZE + 5][MAPSIZE + 5];
 	char* pMapa = &mapa[MAPSIZE][MAPSIZE];
-	int X = 16, Y = 9; // Min position of player is 5, 5
-	int limitScreen = 10;
+	int X = 10, Y = 10; // Min position of player is 5, 5
+	int limitScreen = 11;
 	int* Xpointer = &X;
 	int* Ypointer = &Y;
 	int number = 10;
 	int input = 0;
 	int counterI = 0;
 	int counterJ = 0;
+	int town = 10;
+	int forest = 0;
+	int cave = 0;
+	int league = 0;
+	//To know the number of pokemon captured
+	int captured = 10;
+
+	//TODO: We have to make this work!
+	if (captured >= town)
+	{
+		for (int i = 0; i < MAPSIZE / 4; i++)
+		{
+			mapa[MAPSIZE / 2][i] = ' ';
+		}
+	}
+	if (captured >= forest)
+	{
+		for (int i = MAPSIZE/2; i <MAPSIZE + MAPSIZE / 4; i++)
+		{
+			mapa[i][MAPSIZE/2] = ' ';
+		}
+	}
+	if (captured >= cave)
+	{
+		for (int i = MAPSIZE/2; i < MAPSIZE + MAPSIZE / 4; i++)
+		{
+			mapa[MAPSIZE / 2][i] = ' ';
+		}
+	}
+	if (captured >= league)
+	{
+
+	}
+
+	//Read from files
+	ifstream config;
+	config.open("config.txt", ios::in | ios::app);
+	if (config.is_open())
+	{
+		
+		config >> town;
+		config >> forest;
+		config >> cave;
+
+		std::cout << town << " " << forest << " " << cave <<"\n";
+	}
+
+	
+
+
 
 	//function to prevent player to go outside the map
 	if (X < 5)
@@ -61,6 +113,10 @@ void main()
 		//return map;
 	}
 
+
+	
+
+
 	//TODO: print the pokemons
 	for (int pokemonGenerated = 0; pokemonGenerated < MAXPOKEMON; pokemonGenerated++)
 	{
@@ -71,6 +127,7 @@ void main()
 			mapa[posX][posY] = 'P';
 			std::cout << "Poke in:" << posX << posY << "\n";
 		}
+		std::cout << "Number of Pokémon captured: " << captured;
 
 
 	}
@@ -82,7 +139,7 @@ void main()
 		for (int j = X - 5; j < X + 5; j++)
 		{
 			counterJ++;
-			if (i == MAPSIZE / 2)
+			if (mapa[i][j] == 'X')
 			{
 				mapa[j][i] = 'X';
 				counterI = 0;
@@ -92,7 +149,7 @@ void main()
 			{
 				mapa[j][i] = 'P';
 			}
-			else if (j == MAPSIZE / 2)
+			else if (mapa[i][j] == 'X')
 			{
 				mapa[j][i] = 'X';
 				counterJ = 0;

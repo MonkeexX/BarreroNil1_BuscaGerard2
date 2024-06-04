677d079d8b02fa3c74ecb3f875de396a0b53a2e4
NilBarrero <159423301+NilBarrero@users.noreply.github.com>
2024-06-04 16:45:52 +0200

    Menu battle logic

--- PokeEnti/Capture.h
+++ PokeEnti/Capture.h
@@ -57,8 +57,38 @@ int CapturingPokemon(int posX, int posY, int capturedPoke, int** map)
 	}
 	if (isTherePokemon)
 	{
-		map[x][y] = ' ';
-		capturedPoke++;
+		Combat();
 	}
 	return capturedPoke;
+}
+
+void Combat()
+{
+	//Capturar Pokémon:
+	if (pokebols < 0)
+	{
+		new int capture = rand() % //vida poquemon;
+			if (capture <= 20)
+			{
+				map[x][y] = ' ';
+				capturedPoke++;
+			}
+	}
+	else
+	{
+		std::cout "You d'ont have pokebals";
+	}
+
+	//Atacar:
+	if (life > 0)
+	{
+		//hacer daño
+		if (life < 1)
+		{
+			map[x][y] = ' ';
+		}
+	}
+
+	//Huir:
+	//quitae menu pelea
 }
